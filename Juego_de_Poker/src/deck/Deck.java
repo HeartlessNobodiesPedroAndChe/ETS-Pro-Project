28371a798f4f244f59183a002c335e0c9730ae97
1dama <1dama@PC22122.informatica.edu>
2018-02-22 16:42:33 +0000

    Add author name

--- Juego_de_Poker/src/deck/Deck.java
+++ Juego_de_Poker/src/deck/Deck.java
@@ -3,25 +3,27 @@ package deck;
 import java.util.Arrays;
 
 /**
- * 
+ *
  * @version 0.1 Early
  * @author Becerra Gutiérrez, Jesús Daniel
  * @author Suárez Delgado, Yared
  * @author Núñez Delgado, Eleazar
+ * @author Borges Santamaría, Pedro
  */
-
 // Antes que nada recordad añadir vuestros nombres a @author
 public class Deck {
+
     int[][] deck = new int[4][13];
-    
+
     public Deck() {
         this.deck = load_deck();
         System.out.println(watch_deck(deck));
     }
-    
-    
+
     /**
-     * This method sets values to the attribute deck once Deck Object is initializated.
+     * This method sets values to the attribute deck once Deck Object is
+     * initializated.
+     *
      * @return deck_loaded as <code>Integer</code>
      */
     private static int[][] load_deck() {
@@ -35,9 +37,9 @@ public class Deck {
         }
         return deck_loaded;
     }
-    
+
     private String watch_deck(int[][] deck) {
         return Arrays.deepToString(deck);
     }
-    
+
 }
--- Juego_de_Poker/src/main/Main.java
+++ Juego_de_Poker/src/main/Main.java
@@ -3,23 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package main;
+
 import deck.Deck;
 
 /**
- * 
+ *
  * @author Becerra Gutiérrez, Jesús Daniel
  * @author Suárez Delgado, Yared
  * @author Núñez Delgado, Eleazar
+ * @author Borges Santamaría, Pedro
  */
-
 // This class will be the launcher
 public class Main {
-    
+
     public static void main(String[] args) {
         // Initializating deck object
         Deck deck = new Deck();
     }
-    
+
 }
